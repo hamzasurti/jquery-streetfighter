910242be6dfeec4fa7d3d1ae3ae4f9d45561dd53
hamzasurti <hamza.surti1@gmail.com>
2014-09-16 21:46:32 -0700

    page

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();  
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '1000px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '530px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+
+  $(document).keydown(function(e) {
+	if (e.keyCode == 88) {
+     	$('.ryu-ready').hide();
+      	$('.ryu-still').hide();
+   		$('.ryu-cool').show();
+    }   
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 88) {
+        $('.ryu-cool').hide();
+      	$('.ryu-ready').show();
+    }
+	
+});	
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
